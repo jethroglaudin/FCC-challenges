0a7aad1ada35bb078e73faeead148f10efa6306c
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-18 18:53:37 +0000

    Diff Two Arrays

---
+++ IntermediateAlgorithmScripting/Challenge02.js
@@ -0,0 +1,31 @@
+// Intermediate Algorithm Scripting: Diff Two Arrays
+
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// Note
+// You can return the array with its elements in any order.
+
+
+
+
+/////////////////////// CODE BELOW /////////////////////////////////////////////
+
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  for (var i = 0; i < arr1.length; i++){
+    if (arr2.indexOf(arr1[i])=== -1){
+      newArr.push(arr1[i]);
+    }
+  }
+  for (var j = 0; j < arr2.length; j++){
+    if (arr1.indexOf(arr2[j])=== -1){
+      newArr.push(arr2[j]);
+    }
+  }
+  // Same, same; but different.
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
