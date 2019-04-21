e99a442af4fcd110153a7776b167ff4b1715e0ec
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-21 22:50:31 +0000

    Updated-Pig Latin

--- IntermediateAlgorithmScripting/Challenge06.js
+++ IntermediateAlgorithmScripting/Challenge06.js
@@ -10,18 +10,20 @@
 // Input strings are guaranteed to be English words in all lowercase.
 
 function translatePigLatin(str) {
-  var firstWord=str[0];
-  var vowels=/[aeiou]/gi;
-  var newStr="";
+  let firstWord=str[0];
+  let vowels=/[aeiou]/gi;
+  let newStr="";
   if(!str.match(vowels)){
     return str + 'ay';
-  }
-  if(firstWord.match(vowels)){
-    newStr= str+'way';
+  } else if(firstWord.match(vowels)){
+     newStr= str+'way';
+
   }else{
-    var indexOfVowel=str.indexOf(str.match(vowels)[0]);
+    let indexOfVowel=str.indexOf(str.match(vowels)[0]);
     newStr=str.substr(indexOfVowel)+str.substr(0,indexOfVowel)+'ay';
   }
   return newStr;
 }
+
+
 translatePigLatin("consonant");
