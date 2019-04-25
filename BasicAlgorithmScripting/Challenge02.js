8f0f952b5870dca2d3ff1cd56bc3e76875356d4c
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-04-25 19:19:13 +0000

    Reverse a String

---
+++ BasicAlgorithmScripting/Challenge02.js
@@ -0,0 +1,17 @@
+// Basic Algorithm Scripting: Reverse a String
+
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function reverseString(str) {
+  let newStr = str.split('');
+  let newStrR = newStr.reverse();
+  return newStrR.join('');
+}
+
+reverseString("hello");
