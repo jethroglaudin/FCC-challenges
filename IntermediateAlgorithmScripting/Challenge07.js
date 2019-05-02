f64d92296f2bda43864d5a6d6ec87fccf83cc63d
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-02 16:21:30 +0000

    Search and Replace - refactored

--- IntermediateAlgorithmScripting/Challenge07.js
+++ IntermediateAlgorithmScripting/Challenge07.js
@@ -16,12 +16,9 @@
 
 function myReplace(str, before, after) {
  if (before.charAt(0)=== before.charAt(0).toUpperCase()){
-   str = str.replace(before, after.charAt(0).toUpperCase(0)+ after.slice(1));
- } else{
-  str = str.replace(before, after)
-  }
-  
-  return str;
+   after =  after.charAt(0).toUpperCase(0)+ after.slice(1);
+ } 
+ return str.replace(before, after)
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
