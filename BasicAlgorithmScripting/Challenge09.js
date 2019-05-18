d3ada430e3fdaa2da95e8f72ba2c3e16791c4cd5
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-05-17 21:10:29 -0400

    Finders Keepers

---
+++ BasicAlgorithmScripting/Challenge09.js
@@ -0,0 +1,18 @@
+// Basic Algorithm Scripting: Finders Keepers
+
+// Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+function findElement(arr, func) {
+  let num = arr.filter(func);
+  if (num.length > 1){
+    return num[0];
+  } else {
+    return undefined;
+  }
+
+
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
