a774515cd1821b727374a41ccb2107a89fcbd398
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-20 18:45:16 -0400

    updated js file

--- AdvancedAlgorithms/RomanNumeralConverter.js
+++ AdvancedAlgorithms/RomanNumeralConverter.js
@@ -1,38 +1,28 @@
 function convertToRoman(num) {
     let romanNumerals = ['I', 'IV', 'V', 'IX', 'X', 'XL','L', 'XC', 'C', 'CD', 'D', 'CM', 'M']; 
-    let romanEquavalent = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 100];
+    let romanEquavalent = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
-    let value = null;
+    let value = [];
 
     let romanLength = romanNumerals.length;
     let romanDec = romanEquavalent.length;
 
-    console.log("romanNumeral array length: " + romanLength + "");
-    console.log("romanEqualvalent array length: " + romanLength + "");
+    // console.log("romanNumeral array length: " + romanLength + "");
+    // console.log("romanEqualvalent array length: " + romanDec + "");
 
 
     // check the number based on the roman Equivalent
     // Try to find where the value is nearest. so if we get 30 get 5
 
-    // for(let i = 0; i <= romanEquavalent.length; i++){
-    //     if (num == romanEquavalent[i]){
-    //         value = romanEquavalent[i];
-    //     }
-    // }
+   
 
-    // console.log(value);
+    
+    
+     
 
      return num;
 
 }
 
-convertToRoman(40);
+convertToRoman(50);
 
-// function createNumArray(value) {
-//     let arr = []
-//     for (let a = 1; a <= value; a++) {
-//         arr.push(a)
-//     }
-//     return arr;
-    
-// }
