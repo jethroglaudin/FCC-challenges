f35de321dea41abbf248e9cc8a489e4090971f4e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-23 18:06:59 -0400

    Challenge 4

---
+++ BasicAlgorithmScripting/findTheLongestWordInAString.js
@@ -0,0 +1,14 @@
+function findLongestWordLength(str) {
+    let max = 0;
+    let strArr = str.split(" ");
+    
+    for (let i = 0; i < strArr.length; i++){
+      if (strArr[i].length > max){
+        max = strArr[i].length;
+      }
+    }
+    console.log(max)
+    return max;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
