ae0f49d54fe483818e085133754daf0ce3c6838b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-23 23:03:35 -0400

    Challenge7

---
+++ BasicAlgorithmScripting/repeatAString.js
@@ -0,0 +1,17 @@
+function repeatStringNumTimes(str, num) {
+    // repeat after me
+    let strArr = str.split()
+    console.log(strArr)
+
+    
+    for (let i = 0; i < num; i++){
+        arr.push(str)
+    }
+    arr = arr.join("");
+    console.log(arr);
+    return str;
+
+  
+  }
+  
+  repeatStringNumTimes("abc", 3);
