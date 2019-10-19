e697273a7e6b2eccf47aa2ecb837c85f4b5e2a7e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-18 22:56:48 -0400

    fixed solution

--- IntermediateAlgorithmScripting/DropIt.js
+++ IntermediateAlgorithmScripting/DropIt.js
@@ -1,19 +1,19 @@
 function dropElements(arr, func) {
     // Drop them elements.
   
-    // let length = arr.length;
+    let length = arr.length;
   
-    // for (let i = 0; i < length; i++) {
-    //   if (!func(arr[0])) {
-    //     arr.shift();
-    //   } else {
-    //     break;
-    //   }
-    // }
-  
-    while(!func(arr[0])){
-      arr.shift()
+    for (let i = 0; i < length; i++) {
+      if (!func(arr[0])) {
+        arr.shift();
+      } else {
+        break;
+      }
     }
+  
+    // while(!func(arr[0])){
+    //   arr.shift()
+    // }
     return arr
   }
   
