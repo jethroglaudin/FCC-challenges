f8736a71e9fe9746477250416089c7c7855eafb3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-18 23:32:23 -0400

    Completed Steamroller challenge

--- IntermediateAlgorithmScripting/Steamroller.js
+++ IntermediateAlgorithmScripting/Steamroller.js
@@ -1,19 +1,20 @@
 function steamrollArray(arr) {
   // I'm a steamroller, baby
+  let i = 0;
 
-  let newArr = [];
-  function flatten(arr){
-      return arr.reduce((prev, curr) => {
-        if(Array.isArray(curr))
-            prev.concat(flatten(curr))
-         else
-         prev.concat(curr)
-      })
+  while(i < arr.length){
+    if(Array.isArray(arr)){
+      arr = arr.flat()
+      console.log(arr)
+    }
+    i++;
   }
 
-flatten(arr)
-console.log(arr)
+ 
+  
+  // return arr.flat(Infinity)
   return arr;
+
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
