2cfbb9fe4da4e1ddcde943e3aaf8f5e10a6952d7
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-24 18:48:42 -0400

    DNA Scripting challenge completed

---
+++ FCC-T/DNAScripting.js
@@ -0,0 +1,24 @@
+function pairElement(str) {
+  let dnaArr = [];
+
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case "A":
+        dnaArr.push(["A", "T"]);
+        break;
+      case "T":
+        dnaArr.push(["T", "A"]);
+        break;
+      case "C":
+        dnaArr.push(["C", "G"]);
+        break;
+      case "G":
+        dnaArr.push(["G", "C"]);
+    }
+  }
+  console.log(dnaArr);
+
+  return dnaArr;
+}
+
+pairElement("GCG");
