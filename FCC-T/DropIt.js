4e1e01b7fa4b8a5fc75a85eebdd98cffe52106eb
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-04 18:58:05 -0500

    Challenge Complete

--- FCC-T/DropIt.js
+++ FCC-T/DropIt.js
@@ -1,17 +1,21 @@
 function dropElements(arr, func) {
     // Drop them elements.
-  
     let length = arr.length;
-    for (let i = 0; i < length; i++){
+    for(let i = 0; i < length; i++){
       if(!func(arr[0])){
         arr.shift();
       } else {
         break;
       }
     }
+    // console.log(arr)
+    // while(!func(arr[0])){
+    //   arr.shift();
+    // }
+    // console.log(arr)
     return arr;
   }
   
   dropElements([1, 2, 3, 4], function(n) {return n > 5;})
   
-  // 2:59
+  // T 5:16
