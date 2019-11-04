ff296773a0ac7cdc7533a7cce2f80ee194737ec1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-04 18:36:34 -0500

    Smallest Common multiple challenge complete. Did two seperate solutions.

---
+++ FCC-T/smallestCommonMultiple.js
@@ -0,0 +1,40 @@
+function smallestCommons(arr) {
+  // Solution 1
+    function isMultiple(max, min, multiple){
+        for (let i = max; i >= min; i--){
+            if (multiple % i !== 0){
+                return false;
+            }
+        }
+        return true
+
+    }
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let multiple = max;
+
+    while(!isMultiple(max, min, multiple)){
+        multiple+= max;
+    }
+    console.log(multiple);
+
+    // Solution 2
+
+      // let max = Math.max(...arr);
+    // let min = Math.min(...arr);
+    // let multiple = max;
+    // for (let i = max; i >= min; i--){
+    //     if(multiple % i !== 0){
+    //         multiple += max;
+    //         // console.log(multiple);
+    //         i = max;
+    //     }
+    // }
+    // console.log(multiple);
+   
+    return multiple;
+  }
+  
+  
+  smallestCommons([1,5]);
+  // T 7:05;
