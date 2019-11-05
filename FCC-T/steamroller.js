06f71d563289e4d68ce02bf73643ba94f5512bd8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-04 19:25:19 -0500

    Steamroller challenge completed

---
+++ FCC-T/steamroller.js
@@ -0,0 +1,23 @@
+function steamrollArray(arr) {
+    // I'm a steamroller, baby
+    // return arr.flat(Infinity);
+
+    const flatten = (value) => {
+        return value.flat();
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr)){
+            // 
+          arr = flatten(arr)  
+        } 
+        console.log(arr[i])
+        
+    }
+    console.log(arr);
+    return arr;
+
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
+  
